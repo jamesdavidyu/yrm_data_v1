ed1d3b42289ebaa60f5d0db04b3703a1bb5eb0fb
You Young June <youngjune@planorban.com>
2024-06-19 04:47:31 -0400

    added not working yet login button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
+import LoginButton from "./_components/login_button";
 
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Test
+      <LoginButton />
     </main>
   );
 }
