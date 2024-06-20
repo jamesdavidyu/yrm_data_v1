2ba7bf18640fe7856a6b63c371f90100c25736af
You Young June <youngjune@planorban.com>
2024-06-19 21:33:14 -0400

    created yrm_data postgresql database in vercel and drizzle

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -3,7 +3,10 @@
 
 import { sql } from "drizzle-orm";
 import {
+  date,
+  decimal,
   index,
+  integer,
   pgTableCreator,
   serial,
   timestamp,
@@ -18,17 +21,63 @@ import {
  */
 export const createTable = pgTableCreator((name) => `yrm_data_${name}`);
 
-export const posts = createTable(
-  "post",
+export const categories = createTable(
+  "category",
   {
-    id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
+    category_id: serial("category_id").primaryKey(),
+    category: varchar("category", {length:256}).notNull()
+  }
+);
+
+export const people = createTable(
+  "person",
+  {
+    name_id: serial("name_id").primaryKey(),
+    first_name: varchar("first_name", {length:256}).notNull(),
+    last_name: varchar("last_name", {length:256}).notNull(),
+    dob: date("dob"),
+    rate: decimal("rate")
+  }
+);
+
+export const roles = createTable(
+  "role",
+  {
+    role_id: serial("role_id").primaryKey(),
+    role: varchar("role", {length:256}).notNull()
+  }
+);
+
+export const activities = createTable(
+  "activity",
+  {
+    activity_id: serial("activity_id").primaryKey(),
+    activity: varchar("activity", {length:256}).notNull()
+  }
+);
+
+export const records = createTable(
+  "record",
+  {
+    record_id: serial("record_id").primaryKey(),
+    date: date("date"),
+    category_id: integer("category_id")
+      .references(() => categories.category_id)
       .notNull(),
-    updatedAt: timestamp("updatedAt", { withTimezone: true }),
+    name_id: integer("name_id")
+      .references(() => people.name_id)
+      .notNull(),
+    role_id: integer("role_id")
+      .references(() => roles.role_id)
+      .notNull(),
+    activity_id: integer("activity_id")
+      .references(() => activities.activity_id)
+      .notNull(),
+    hours: decimal("hours"),
+    money: decimal("money"),
+    comments: varchar("comments", { length: 256 })
   },
   (example) => ({
-    nameIndex: index("name_idx").on(example.name),
+    hoursIndex: index("hours_idx").on(example.hours),
   })
 );
