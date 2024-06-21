52d6024b2cdf5b3894211e7f6937c3b2c6ca4487
You Young June <youngjune@planorban.com>
2024-06-20 20:00:37 -0400

    got project ready to import data from old db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,9 @@ export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <SignedOut>
-        <SignInButton />
+        <div className="border p-4">
+          Hi Lou, <SignInButton />
+        </div>
       </SignedOut>
       <SignedIn>
         <UserButton />
