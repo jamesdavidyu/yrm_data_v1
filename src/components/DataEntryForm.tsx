46c5332fef5d6cfc490e6cb09994c16ce51819ed
You Young June <youngjune@planorban.com>
2024-06-22 00:14:48 -0400

    added button to add rows; need to fix so number of rows refreshes to orignial when page refreshes

--- src/components/DataEntryForm.tsx
+++ src/components/DataEntryForm.tsx
@@ -2,13 +2,18 @@ import { DataEntryFormSubmit } from "./DataEntryFormSubmit";
 import { DataEntryRow } from "./DataEntryRow";
 import "~/styles/globals.css";
 
+var defaultRows: number = 10;
+
 export function DataEntryForm() {
   return (
     <div className="container p-4">
       <div className="columns"></div>
       <div className="columns">
         <form>
-          <DataEntryRow />
+          {Array.from({ length: defaultRows }).map((_, index) => (
+            <DataEntryRow key={index} />
+          ))}
+          <AddRowButton />
           <div className="container">
             <div className="columns"></div>
             <div className="columns flex items-center justify-center">
@@ -21,4 +26,16 @@ export function DataEntryForm() {
       <div className="columns"></div>
     </div>
   );
+}
+
+function AddRowButton() {
+  return (
+    <button className="rounded-full border p-1" onClick={AddRow()}>
+      +
+    </button>
+  );
+}
+
+function AddRow(): any {
+  defaultRows++;
 }
